798123b69c41c0343c0b75a920fd6fdeab4a85c7
Daniel <henio09@wp.pl>
2023-05-18 17:30:38 +0200

    added homepage prototype

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ export default function Home() {
   return (
     <>
       <header className='flex flex-row justify-between'>
+        <img src="/homepage/hero-background.jpg" alt="background-image"
+        />
         <a href="/"><img
           src="/next.svg"
           alt="main_logo"
@@ -21,11 +23,47 @@ export default function Home() {
         </nav>
       </header>
       <main className={`flex justify-center`}>
-        <div className="container flex flex-col justify-center items-center h-screen">
-          <h1 className='mb-10 text-xl'>Site is under construction</h1>
-          <Button variant="outlined" href="/content">Content</Button>
+        <div className="container">
+          <div className="content">
+            <a href="/linked-list" className="content__item-link">
+              <div className="content__item">
+                <img className="content__image" src="http://placehold.it/300x200" />
+                <div className="content__info-container">
+                  <p>LINKED LIST</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="/array" className="content__item-link">
+              <div className="content__item">
+                <img className="content__image" src="http://placehold.it/300x200" />
+                <div className="content__info-container">
+                  <p>ARRAY</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="/stack" className="content__item-link">
+              <div className="content__item">
+                <img className="content__image" src="http://placehold.it/300x200" />
+                <div className="content__info-container">
+                  <p>STACK</p>
+                </div>
+              </div>
+            </a>
+
+            <a href="/queue" className="content__item-link">
+              <div className="content__item">
+                <img className="content__image" src="http://placehold.it/300x200" />
+                <div className="content__info-container">
+                  <p>QUEUE</p>
+                </div>
+              </div>
+            </a>
+          </div>
         </div>
       </main>
+      <footer className='mt-10'></footer>
     </>
   )
 }
